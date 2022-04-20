833e66b97de6446e6886cfb1bcbdb39bca2d9102
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-04-20 22:44:50 +0530

    file added

---
+++ Consumer.java
@@ -0,0 +1,21 @@
+
+public class Consumer extends Thread{
+	private Shop Shop;
+    private int number;
+    public Consumer(Shop c, int number)
+    {
+          Shop = c;
+          this.number = number;
+    }
+    public void run()
+    {
+          int value = 0;
+          for (int i = 0; i < 10; i++)
+          {
+                value = Shop.get();//0
+                System.out.println("Consumed value " + this.number+ " got: " + value);
+          }
+    }
+
+
+}
